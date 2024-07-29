87d4af2953dfee108b0dc18862caafdbf4234ba2
Valknut-Warrior <164926390+Valknut-Warrior@users.noreply.github.com>
2024-07-29 17:06:34 +0200

    Update 0.0.4
    Add Load More Button

--- main.js
+++ main.js
@@ -1,129 +1,133 @@
 "use strict";
 
+const loadingIndicator = document.getElementById("loading");
+const pokemonContainer = document.getElementById("pokemon-container");
+let currentOffset = 0; // Startwert für Offset
+
 document.addEventListener("DOMContentLoaded", function () {
-  const loadingIndicator = document.getElementById("loading");
-  const pokemonContainer = document.getElementById("pokemon-container");
+  // Beispielaufruf der Funktionen
+  (async function () {
+    await PokemonsAll(16, currentOffset);
+  })();
+});
 
-  // Show Spinner
-  function showLoading() {
-    loadingIndicator.style.display = "block";
+// Show Spinner
+async function showLoading() {
+  loadingIndicator.style.display = "block";
+}
+
+// Hide Spinner
+async function hideLoading() {
+  loadingIndicator.style.display = "none";
+}
+
+// Hole alle Pokemons aus der API
+async function PokemonsAll(limit = 16, offset = 0) {
+  try {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+    let responseAllPokemonAsJson = await response.json();
+
+    let pokemonData = responseAllPokemonAsJson.results.map((pokemon) => ({
+      name: pokemon.name,
+      url: pokemon.url,
+    }));
+
+    // console.log("Alle Pokemon-Namen und URLs:", pokemonData);
+
+    // Hier rufen wir getPokeDetails für jedes Pokémon auf
+    let allPokemonDetails = await Promise.all(pokemonData.map((pokemon) => getPokeDetails(pokemon)));
+
+    // Hier rufen wir renderPokemonCards mit allen Details auf
+    renderPokemonCards(allPokemonDetails);
+
+    // Verstecke den Ladeindikator erst hier, nachdem alle Daten geladen und die Karten gerendert wurden
+    hideLoading();
+  } catch (error) {
+    hideLoading();
+    console.error("Fehler beim Laden der Pokemon:", error);
+  }
+}
+
+async function getPokeDetails(pokemon) {
+  try {
+    // Teste die Ausgabe von Pokemon Details
+    // console.log("Pokemon Details:", pokemon.url);
+
+    let response = await fetch(pokemon.url);
+    let pokemonDetails = await response.json();
+
+    // console.log("Fetched Pokemon Details:", pokemonDetails);
+
+    // Rückgabe der Details zusammen mit dem ursprünglichen Namen
+    return { ...pokemonDetails, name: pokemon.name };
+  } catch (error) {
+    console.error("Fehler beim Laden der Pokemon Details:", error);
+  }
+}
+
+function renderPokemonCards(pokemonData) {
+  if (!pokemonData || !Array.isArray(pokemonData)) {
+    console.error("pokemonData ist ungültig:", pokemonData);
+    return;
   }
 
-  // Hide Spinner
-  function hideLoading() {
-    loadingIndicator.style.display = "none";
-  }
+  pokemonContainer.innerHTML = ""; // Leere den Container
 
-  // Hole alle Pokemons aus der API
-  async function PokemonsAll() {
-    try {
-      showLoading();
+  for (let i = 0; i < pokemonData.length; i++) {
+    // console.log("Pokemon Name:", pokemonData[i].name, "Details:", pokemonData[i]);
 
-      let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
-      let responseAllPokemonAsJson = await response.json();
-      // hideLoading();
+    let PokeName = pokemonData[i].name;
+    PokeName = String(PokeName).charAt(0).toUpperCase() + PokeName.slice(1);
+    PokeName = PokeName.replace("-f", "_F");
+    PokeName = PokeName.replace("-m", "");
+    PokeName = PokeName.replace("Mrime", "MrMime");
 
-      let pokemonData = responseAllPokemonAsJson.results.map((pokemon) => ({
-        name: pokemon.name,
-        url: pokemon.url,
-      }));
+    let { id, height, weight, types, stats } = pokemonData[i];
+    let type = types.map((t) => t.type.name).join(" / ");
+    let hp = stats.find((stat) => stat.stat.name === "hp").base_stat;
+    let idHTML = id.toString().padStart(3, "0");
 
-      // console.log("Alle Pokemon-Namen und URLs:", pokemonData);
+    // Großschreibung der Anfangsbuchstaben der Typen
+    let typeInfo = types.map((x) => x.type.name.charAt(0).toUpperCase() + x.type.name.slice(1)).join(" / ");
 
-      // Hier rufen wir getPokeDetails für jedes Pokémon auf
-      let allPokemonDetails = await Promise.all(pokemonData.map((pokemon) => getPokeDetails(pokemon)));
+    // Erstellen Sie die CSS-Klasse für den Farbverlauf basierend auf den Typen
+    let typeClasses = types.map((t) => t.type.name);
+    let gradientClass = typeClasses.length > 1 ? `${typeClasses[0]}-${typeClasses[1]}-gradient` : `${typeClasses[0]}`;
 
-      // Hier rufen wir renderPokemonCards mit allen Details auf
-      renderPokemonCards(allPokemonDetails);
+    // Erstellen Sie die CSS-Klasse für den Rand basierend auf dem ersten Typ
+    let typeClass = `${types[0].type.name}-border-card`;
 
-      hideLoading();
-    } catch (error) {
-      hideLoading();
-      console.error("Fehler beim Laden der Pokemon:", error);
-    }
-  }
-
-  async function getPokeDetails(pokemon) {
-    try {
-      showLoading();
-
-      // Teste die Ausgabe von Pokemon Details
-      // console.log("Pokemon Details:", pokemon.url);
-
-      let response = await fetch(pokemon.url);
-      let pokemonDetails = await response.json();
-
-      hideLoading();
-
-      // console.log("Fetched Pokemon Details:", pokemonDetails);
-
-      // Rückgabe der Details zusammen mit dem ursprünglichen Namen
-      return { ...pokemonDetails, name: pokemon.name };
-    } catch (error) {
-      hideLoading();
-      console.error("Fehler beim Laden der Pokemon Details:", error);
-    }
-  }
-
-  function renderPokemonCards(pokemonData) {
-    if (!pokemonData || !Array.isArray(pokemonData)) {
-      console.error("pokemonData ist ungültig:", pokemonData);
-      return;
-    }
-
-    pokemonContainer.innerHTML = ""; // Leere den Container
-
-    for (let i = 0; i < pokemonData.length; i++) {
-      // console.log("Pokemon Name:", pokemonData[i].name, "Details:", pokemonData[i]);
-
-      let PokeName = pokemonData[i].name;
-      PokeName = String(PokeName).charAt(0).toUpperCase() + PokeName.slice(1);
-      PokeName = PokeName.replace("-f", "_F");
-      PokeName = PokeName.replace("-m", "");
-      PokeName = PokeName.replace("Mrime", "MrMime");
-
-      let { id, height, weight, types, stats } = pokemonData[i];
-      let type = types.map((t) => t.type.name).join(" / ");
-      let hp = stats.find((stat) => stat.stat.name === "hp").base_stat;
-      id = id.toString().padStart(3, "0");
-
-      let cssTypes = type.split("/");
-
-      // Hier müssen noch die Typen rausgezogen werden für die CSS Regeln
-      if (type && cssTypes[0]) {
-        console.log("ID", i, ":", "Type", cssTypes);
-      }
-
-      pokemonContainer.innerHTML += `    
-        <div class="card has-text-weight-bold has-text-white pokemon">
+    pokemonContainer.innerHTML += `    
+        <div class="card has-text-weight-bold has-text-white pokemon ${typeClass}">
         <div class="card-header">
-        <div class="id">#${id}</div>
+        <div class="id">#${idHTML}</div>
           </div>
           <div class="card-image">
-            <div class="card-image-container">
-            <img src="http://static.pokemonpets.com/images/monsters-images-300-300/${i + 1}-${PokeName}.webp" alt="${PokeName}" />
+            <div class="card-image-container ${gradientClass}">
+            <img src="http://static.pokemonpets.com/images/monsters-images-300-300/${id}-${PokeName}.webp" alt="${PokeName}" />
             </div>
-            <div class="type"></div>
           </div>
           <div class="card-content has-text-centered">
             <div class="">
               <div class="title">${PokeName}</div>
-              <div class="hp">hp ${hp}</div>
+              <div class="hp">HP ${hp}</div>
             </div>
             <div class="stats columns is-mobile">
-              <div class="column has-text-centered">${type}<br><span class="tag">Type</span></div>
-              <div class="column has-text-centered center-column">${weight / 10} Kg<br><span
-                class="tag">Weight</span></div>
-              <div class="column has-text-centered">${height / 10} m<br><span class="tag">Height</span></div>
+          <div class="extra-info">
+                <div> <small> Weight</small> <h5 class="weight"> ${weight / 10} Kg </h5></div>
+                <div class="space"></div>
+                <div> <small> Height</small> <h5 class="height"> ${height / 10} m</h5></div>      
+            </div>
+            <div class="type">${typeInfo}</div>
             </div>
           </div>
         </div>
       `;
-    }
   }
+}
 
-  // Beispielaufruf der Funktionen
-  (async function () {
-    await PokemonsAll();
-  })();
-});
+// Funktion zum Laden von mehr Pokémon
+async function loadMorePokemons() {
+  currentOffset += 16; // Erhöhe den Offset um 20 (oder eine andere gewünschte Anzahl)
+  await PokemonsAll(16, currentOffset); // Lade weitere Pokémon
+}
